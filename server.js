f696ce6d26a3d7bcf38669764d40486fde256308
RithvikC-png <rithvikc@ad.unc.edu>
2022-11-27 23:42:29 -0500

    Test 6

--- server.js
+++ server.js
@@ -1,55 +1,91 @@
-#!/usr/bin/env node
+#!/use/bin/env node
 
+import {roll} from "./lib/roll.js"
+import express from 'express';
 import minimist from "minimist";
-import express from "express";
-import { roll } from "./lib/roll.js";
 
-const app = express();
 
-const argument = minimist(process.argv.slice(2));
+const app = express()
+app.use(express.urlencoded({extended: true}));
 
-const port = (() => {
-    if (argument.port != null) {
-        return argument.port;
-    }
-    else {
-        return 5000;
-    }
-})();
+const args = minimist(process.argv.slice(2))
+const port = args.port || 5000;
 
-app.listen(port, () => {
-    console.log("Server listening on port " + port);
+
+app.get('/app/', (req, res) => {
+    // console.log('rooty tooty');
+    res.status(200).send("200 OK");
 })
 
 
-app.get('/app/', (req, res, next) => {
-    res.status(200).send('200 OK');
-});
-
-app.use(express.urlencoded({extended: true}));
-
 app.get('/app/roll/', (req, res, next) => {
-    // Default roll = 6 sides, 2 die, and 1 roll
-    res.status(200).send(JSON.stringify(roll(6, 2, 1)));
-});
+    let sides = 6;
+    let dice = 2;
+    let rolls = 1;
+    res.status(200).send(roll(sides, dice, rolls));
+})
 
 app.post('/app/roll/', (req, res, next) => {
-    res.status(200).send(JSON.stringify(roll(parseInt(req.body.sides), parseInt(req.body.dice), parseInt(req.body.rolls))));
-});
+    let sides = 6;
+    let dice = 2;
+    let rolls = 1;
+
+    if (req.body.sides) {
+        sides = parseInt(req.body.sides);
+    }
+    if (req.body.dice) {
+        dice = parseInt(req.body.dice);
+    }
+    if (req.body.rolls) {
+        rolls = parseInt(req.body.rolls);
+    }
+    res.status(200).send(roll(sides, dice, rolls));
+})
+
 
 app.get('/app/roll/:sides/', (req, res, next) => {
-    res.status(200).send(JSON.stringify(roll(parseInt(req.params.sides), 2, 1)));
-});
+    let sides = 6;
+
+    if (req.params.sides) {
+        sides = parseInt(req.params.sides);
+    }
+    res.status(200).send(roll(sides, 2, 1));
+})
+
 
 app.get('/app/roll/:sides/:dice/', (req, res, next) => {
-    res.status(200).send(JSON.stringify(roll(parseInt(req.params.sides), parseInt(req.params.dice), 1)));
-});
+    let sides = 6;
+    let dice = 2;
+
+    if (req.params.sides) {
+        sides = parseInt(req.params.sides);
+    }
+    if (req.params.dice) {
+        dice = parseInt(req.params.dice);
+    }
+    res.status(200).send(roll(sides, dice, 1));
+})
+
 
 app.get('/app/roll/:sides/:dice/:rolls/', (req, res, next) => {
-    res.status(200).send(JSON.stringify(roll(parseInt(req.params.sides), parseInt(req.params.dice), parseInt(req.params.rolls))));
-});
+    let sides = 6;
+    let dice = 2;
+    let rolls = 1;
 
+    if (req.params.sides) {
+        sides = parseInt(req.params.sides);
+    }
+    if (req.params.dice) {
+        dice = parseInt(req.params.dice);
+    }
+    if (req.params.rolls) {
+        rolls = parseInt(req.params.rolls);
+    }
+    res.status(200).send(roll(sides, dice, rolls));
+})
 
-app.get('*', (req, res, next) => {
-    res.status(404).send('404 NOT FOUND');
-});
+app.get('*', (req, res) => {
+    res.status(404).send("404 NOT FOUND");
+})
+
+app.listen(port);
