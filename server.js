8da0e8c395a9ac75cae0e02c5736ee86d9851f66
RithvikC-png <rithvikc@ad.unc.edu>
2022-11-27 23:15:47 -0500

    Test 5

--- server.js
+++ server.js
@@ -39,3 +39,7 @@ app.get('/app/roll/:sides/:dice', (req, res, next) => {
 app.get('/app/roll/:sides/:dice/:rolls', (req, res, next) => {
     res.status(200).send(JSON.stringify(roll(parseInt(req.params.sides), parseInt(req.params.dice), parseInt(req.params.rolls))));
 });
+
+app.get('*', (req, res, next) => {
+    res.status(404).send("404 Not Found");
+});
